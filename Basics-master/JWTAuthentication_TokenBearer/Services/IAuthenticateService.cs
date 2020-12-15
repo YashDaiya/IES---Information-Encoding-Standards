5bf9c1c2919a1182c9b023d67196d0afd1dbf2b6
YashDaiya <72169150+YashDaiya@users.noreply.github.com>
2020-12-14 19:59:38 -0500

    Uploading

---
+++ Basics-master/JWTAuthentication_TokenBearer/Services/IAuthenticateService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JWTAuthentication_TokenBearer;
+
+namespace JWTAuthentication_TokenBarer.Services
+{
+    public interface IAuthenticateService
+    {
+        Student Authenticate(String UserName, String Password);
+    }
+}
